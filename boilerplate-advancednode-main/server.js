508549b91787b11e94ad2c6500f6c853196ba0c8
Pedro <peedrohs.mesquita@outlook.com>
2023-01-22 14:34:23 -0300

    Advanced Node and Express in progress

---
+++ boilerplate-advancednode-main/server.js
@@ -0,0 +1,24 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const myDB = require('./connection');
+const pug = require('pug');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+
+const app = express();
+
+fccTesting(app); //For FCC testing purposes
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.route('/').get((req, res) => {
+    res.render('index', {'title': 'Hello', 'message': 'Please log in'});
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Listening on port ' + PORT);
+});
