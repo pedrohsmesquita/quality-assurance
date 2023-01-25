2aaaef753c17870bc59140d7154494ee39623d47
Pedro <peedrohs.mesquita@outlook.com>
2023-01-25 15:02:44 -0300

    Advanced Node and Express in progress

--- boilerplate-advancednode-main/server.js
+++ boilerplate-advancednode-main/server.js
@@ -32,8 +32,25 @@ myDB(async client => {
         res.render('index', {
             title: 'Home page',
             message: 'Please login',
-            showLogin: true
+            showLogin: true,
+            showRegistration: true
         });
+    });
+
+    app.route('/register').post((req, res, next) => {
+        db.findOne({username: req.body.username}, (err, user) => {
+            if (err) next(err);
+            else if (user) res.redirect('/');
+            db.insertOne({
+                username: req.body.username,
+                password: req.body.password
+            }, (err, doc) => {
+                if (err) res.redirect('/');
+                else next(null, doc.ops[0]);
+            });
+        });
+    }, passport.authenticate('local', {failureRedirect: '/'}), (req, res, next) => {
+        res.redirect('/profile');
     });
 
     app.route('/login').post(passport.authenticate('local', {failureRedirect: '/'}), (req, res) => {
@@ -42,6 +59,17 @@ myDB(async client => {
         
     app.route('/profile').get(ensureAuthenticated, (req, res) => {
         res.render('profile', {username: req.user.username});
+    });
+
+    app.route('/logout').get((req, res) => {
+        req.logout();
+        res.redirect('/');
+    });
+
+    app.use((req, res, next) => {
+        res.status(404)
+            .type('text')
+            .send('Not Found');
     });
     
     passport.serializeUser((user, done) => {
