46833a5c7a68f6a175e0400b91b9b108b11a6676
Pedro <peedrohs.mesquita@outlook.com>
2023-01-26 15:35:52 -0300

    Implementation of Social Authentication III done

--- boilerplate-advancednode-main/auth.js
+++ boilerplate-advancednode-main/auth.js
@@ -4,19 +4,19 @@ const LocalStrategy = require('passport-local');
 const GitHubStrategy = require('passport-github').Strategy;
 const { ObjectID } = require('mongodb');
 
-module.exports = function (app, db) {
+module.exports = function (app, myDataBase) {
     passport.serializeUser((user, done) => {
         done(null, user._id);
     });
 
     passport.deserializeUser((id, done) => {
-        db.findOne({_id: new ObjectID(id)}, (err, doc) => {
+        myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
             done(null, doc);
         });
     });
 
     passport.use(new LocalStrategy((username, password, done) => {
-        db.findOne({username: username}, (err, user) => {
+        myDataBase.findOne({ username: username }, (err, user) => {
             console.log(`User ${username} has attempted to log in.`);
             if (err) return done(err);
             if (!user) return done(null, false);
@@ -31,5 +31,31 @@ module.exports = function (app, db) {
         callbackURL: 'https://boilerplate-advancednode.pedrohsmesquita.repl.co/auth/github/callback'
     }, (accessToken, refreshToken, profile, cb) => {
         console.log(profile);
+        myDataBase.findOneAndUpdate(
+            { id: profile.id },
+            {
+                $setOnInsert: {
+                    id: profile.id,
+                    username: profile.username,
+                    name: profile.displayName || 'John Doe',
+                    photo: profile.photos[0].value || '',
+                    email: Array.isArray(profile.emails)
+                        ? profile.emails[0].value
+                        : 'No public email',
+                    created_on: new Date(),
+                    provider: profile.provider || ''
+                },
+                $set: {
+                    last_login: new Date()
+                },
+                $inc: {
+                    login_count: 1
+                }
+            },
+            { upsert: true, new: true },
+            (err, doc) => {
+                return cb(null, doc.value);
+            }
+        );
     }));
 }
